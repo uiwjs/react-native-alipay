c49fb0e142ecad423d76f5d494286a8bd4c31547
jaywcjlove <398188662@qq.com>
2022-08-05 17:57:49 +0800

    chore(example): update metro.config.

--- example/metro.config.js
+++ example/metro.config.js
@@ -3,13 +3,19 @@
  * https://github.com/facebook/react-native
  */
 
+const path = require('path');
+
 module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
+  // workaround for an issue with symlinks encountered starting with
+  // metro@0.55 / React Native 0.61
+  // (not needed with React Native 0.60 / metro@0.54)
+  resolver: {
+    extraNodeModules: new Proxy(
+      {},
+      {get: (_, name) => path.resolve('.', 'node_modules', name)},
+    ),
   },
+
+  // quick workaround for another issue with symlinks
+  watchFolders: ['.', '..'],
 };
